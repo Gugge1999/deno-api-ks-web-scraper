0ec801f3ce1729661925a1809898b06119eb6af2
David <davidgust99@gmail.com>
2025-01-02 19:52:35 +0100

    Mer funktioner för jwt i user.ts

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,12 +1,13 @@
 import { httpErrors } from "@oak/oak";
 import { Router } from "@oak/oak/router";
-import { JWTPayload, SignJWT } from "jose";
+import { JWTPayload, jwtVerify, SignJWT } from "jose";
 import { Buffer } from "node:buffer";
 import { randomBytes, scryptSync, timingSafeEqual } from "node:crypto";
 
 import { getUserByEmail, insertNewUser } from "../database/user.ts";
 import { validateBody } from "./bevakningar.ts";
 import { keyLength } from "../constants/config.ts";
+import { errorLogger } from "../services/logger.ts";
 
 // TODO: Den här borde sättas i .env
 const secret = new TextEncoder().encode("secret-that-no-one-knows");
@@ -36,17 +37,28 @@ userRoutes.post(`/register`, async (context) => {
   }
 
   // TODO: Ska det vara en random guid eller id från app_user?
-  const token = await createJWT({ userId: 123, username: "john_doe" });
+  const token = await createJwt({ userId: 123, username: "john_doe" });
 
   context.cookies.set("jwt", token, { httpOnly: true });
 
-  context.response.body = {
-    user: newUser.result?.[0],
-  };
+  context.response.body = newUser.result?.[0];
 });
 
+// TODO: Vid inloggning bör inte jwt finnas. När man loggar in borde den sättas
 userRoutes.post(`/login`, async (context) => {
   const { email, password } = await validateBodyUser(context);
+
+  const jwtToken = await context.cookies.get("jwt");
+
+  if (jwtToken === undefined) {
+    throw new httpErrors.Unauthorized("Ingen jwt token hittades");
+  }
+
+  const jwtVerified = await verifyJwt(jwtToken);
+
+  if (jwtVerified === null) {
+    throw new httpErrors.Unauthorized("Ogiltig jwt token");
+  }
 
   const user = await getUserByEmail(email);
 
@@ -67,22 +79,35 @@ userRoutes.post(`/login`, async (context) => {
   context.response.body = "";
 });
 
+// TODO: Skapa endpoint för att logga ut, glömt lösenord, uppdatera lösenord och radera användare
 function hash(password: string): string {
-  // generate random 16 bytes long salt - recommended by NodeJS Docs
-  const salt = randomBytes(16).toString("hex");
-  const derivedKey = scryptSync(password, salt, keyLength);
+  try {
+    // generate random 16 bytes long salt - recommended by NodeJS Docs
+    const salt = randomBytes(16).toString("hex");
+    const derivedKey = scryptSync(password, salt, keyLength);
 
-  return `${salt}.${derivedKey.toString("hex")}`;
+    return `${salt}.${derivedKey.toString("hex")}`;
+  } catch (e) {
+    errorLogger.error({ message: `Något gick fel vid hashande av lösenord: Error: ${e}` });
+
+    return "";
+  }
 }
 
 function comparePasswords(password: string, hash: string): boolean {
-  const [salt, hashKey] = hash.split(".");
-  const hashKeyBuff = Buffer.from(hashKey, "hex");
-  const derivedKey = scryptSync(password, salt, keyLength);
-  return timingSafeEqual(hashKeyBuff, derivedKey);
+  try {
+    const [salt, hashKey] = hash.split(".");
+    const hashKeyBuff = Buffer.from(hashKey, "hex");
+    const derivedKey = scryptSync(password, salt, keyLength);
+    return timingSafeEqual(hashKeyBuff, derivedKey);
+  } catch (e) {
+    errorLogger.error({ message: `Något gick fel vid jämförande av lösenord: Error: ${e}` });
+
+    return false;
+  }
 }
 
-async function createJWT(payload: JWTPayload): Promise<string> {
+async function createJwt(payload: JWTPayload): Promise<string> {
   const jwt = await new SignJWT(payload)
     .setProtectedHeader({ alg: "HS256" })
     .setIssuedAt()
@@ -90,6 +115,21 @@ async function createJWT(payload: JWTPayload): Promise<string> {
     .sign(secret);
 
   return jwt;
+}
+
+async function verifyJwt(token: string | undefined): Promise<JWTPayload | null> {
+  try {
+    if (token === undefined) {
+      return null;
+    }
+
+    const { payload } = await jwtVerify(token, secret);
+    return payload;
+  } catch (error) {
+    errorLogger.error({ message: `Ogiltig jwt token. dbError: ${error}` });
+
+    return null;
+  }
 }
 
 // TODO: Typa bättre, kanske unknown?
