9cb44a31c2e999fd8e7bb2b4fa887ba1f18e18f1
David <davidgust99@gmail.com>
2025-05-22 19:52:58 +0200

    Uppdatera paket

--- src/database/query.ts
+++ src/database/query.ts
@@ -60,8 +60,6 @@ export async function hejsanTesting(query: PendingQuery<Row[]>[]): Promise<DbRes
 }
 
 function getDb() {
-  console.log("Här");
-
   // OBS: lägg märke till import från dotenv. Den kastar inte fel om import saknas men kommer inte att fungera
   if (Deno.env.get("ENV") === "dev") {
     const url = `postgres://${Deno.env.get("PGUSERNAME")}:${Deno.env.get("PGPASSWORD")}@localhost:5432/${Deno.env.get("PGDATABASE")}`;
@@ -70,8 +68,6 @@ function getDb() {
   }
 
   const prodDbUrl = Deno.env.get("DATABASE_URL") ?? "";
-
-  console.log("prod url: ", prodDbUrl);
 
   return postgres(prodDbUrl, {
     idle_timeout: 20,
